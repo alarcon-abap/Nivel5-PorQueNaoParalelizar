ff5cc0d2827908629cee52672786f0bf53ce7e8d
Rodrigo de O. Alarcon <56162652+alarcon-abap@users.noreply.github.com>
2023-09-14 17:49:45 -0300

    Update PessoaFisica.java

--- CadastroClient/src/model/PessoaFisica.java
+++ CadastroClient/src/model/PessoaFisica.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import java.io.Serializable;
@@ -21,7 +16,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author wfeli
+ * @author AlarconABap
  */
 @Entity
 @Table(name = "PessoaFisica")
@@ -150,7 +145,7 @@ public class PessoaFisica implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        
         if (!(object instanceof PessoaFisica)) {
             return false;
         }
