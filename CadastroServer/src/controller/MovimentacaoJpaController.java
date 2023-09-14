067a322d90c745b1efe05300b5da3f7713dc0473
Rodrigo de O. Alarcon <56162652+alarcon-abap@users.noreply.github.com>
2023-09-14 17:56:12 -0300

    Update MovimentacaoJpaController.java

--- CadastroServer/src/controller/MovimentacaoJpaController.java
+++ CadastroServer/src/controller/MovimentacaoJpaController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import controller.exceptions.NonexistentEntityException;
@@ -21,7 +17,7 @@ import model.Usuario;
 
 /**
  *
- * @author wfeli
+ * @author AlarconAbap
  */
 public class MovimentacaoJpaController implements Serializable {
 
